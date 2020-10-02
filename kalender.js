572082ce550f8409adb6c406dfee5f3767991b57
MikkLehtsaar <mikk.lehtsaar@ametikool.ee>
2020-10-02 11:19:07 +0300

    last commit hopefully

---
+++ kalender.js
@@ -0,0 +1,15 @@
+/*window.onscroll = function() {myFunction()};
+
+var times = document.getElementById("times");
+
+// Get the offset position of the navbar
+var sticky = times.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageXOffset >= sticky) {
+    times.classList.add("sticky")
+  } else {
+    times.classList.remove("sticky");
+  }
+}*/
